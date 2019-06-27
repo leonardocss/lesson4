4eeaa4e7c1763ff9ab3584868fe81e2e888ec778
leonardo <h4k3rik6@gmail.com>
2019-06-27 15:42:55 +0500

    Первое обязательное задание

--- js/arrays.js
+++
@@ -1,8 +0,0 @@
-let array = [[1,2,3,4,5],[35,6,8,4,2,2,556,3],[3,5,6,33,3333,2]];
-let summ = 0;
-for (let i = 0; i < array.length; i++) {
-	for (let j = 0; j < array[i].length; j++) {
-		summ += array[i][j];
-	}
-}
-console.log(summ);
--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
-let options = {
-	name: "Vasya",
-	width: 1024,
-	height: 1280
-}
-//создаем свойство объекту
-options.bool = false;
-options.colors = {
-	background: 'red',
-	border: 'black'
-}
+//Создаем переменные LI - ПЯТЫЙ ПУНКТ с классом menu-item
+let li = document.createElement('li');
+//Создаем переменные LI - ТРЕТИЙ ПУНКТ с классом menu-item
+let li2 = document.createElement('li');
+// вешаем классы
+li.classList.add('menu-item');
+li2.classList.add('menu-item');
+//добавляем туда HTML код (текст внутри LI)
+li.innerHTML = 'Пятый пункт';
+li2.innerHTML = 'Третий пункт';
 
-delete options.bool;
-
-console.log(options);
-
-for (let key in options) {
-	console.log('Свойство ' + key + " имеет значение: " + options[key])
-}
-
-//узнать кол-во свойств в объекте
-console.log(Object.keys(options).length)
-
-
-//операции с массивом 
-let arr = [1,2,3,4,5];
-//удаляет последний эелемент (возвращает удаленый эемент)
-arr.pop();
-console.log(arr.pop()) // получаем удаленный элемент))
-arr.push(6) // добавляет элемент в конец массива - "шестерку"
-console.log(arr + " добавили элемент 6")
-
-arr.shift(); // удаляет элемент в начале массива
-console.log(arr + " удалил 1й элемент в начале")
-arr.unshift(0)
-console.log(arr + " добавили элемент вначале 0")
-
-let first = [1,'second',3,4,5]
-first[99] = 99;
-first.forEach(function(item,i,arr){
-	console.log(i + ": " + item + "(array: " + arr +')')
-})
+// пробиваемся до body > header > nav > ul > и добавляем в конец ПЯТЫЙ ПУНКТ
+document.body.children[2].children[0].children[0].appendChild(li);
+// УДАЛЯЕМ ЛИШНИЙ ЭЛЕМЕНТ
+document.body.children[2].children[0].children[0].removeChild(document.body.children[2].children[0].children[0].children[1])
+// ДОБАВЛЯЕМ ТРЕТИЙ ПЕРЕД ЧЕТВЕРТЫМ
+document.body.children[2].children[0].children[0].insertBefore(li2, document.body.children[2].children[0].children[0].children[2]);
+//МЕНЯЕМ СТИЛЬ У БАДИ - ЗАМЕНА ФОНА ДРУГОЙ КАРТИНКОЙ
+document.body.style.background = 'url(../img/apple_true.jpg) center no-repeat';
+//Поменять заголовок, добавить слово "подлинную"
+title.innerHTML = 'Мы продаем только подлинную технику Apple';
+// удаляем блок рекламы
+document.querySelector('.adv').remove();
+//получам блок с идентификатором prompt и записываем туда HTML CODE (text) через вопрос у пользователя 
+let feedback = document.getElementById('prompt');
+feedback.innerHTML = prompt('Ваше отношение к APPLE???', '');
